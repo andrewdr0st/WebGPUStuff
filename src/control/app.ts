105cc68c386e7b0228d95e912398f4037797bf2f
Andrew Drost <anddrost@iastate.edu>
2024-07-11 15:49:10 -0500

    first person camera

---
+++ src/control/app.ts
@@ -0,0 +1,77 @@
+import { Renderer } from "../view/renderer";
+import { Scene } from "../models/scene";
+import $, { event } from "jquery"
+
+export class App {
+    canvas: HTMLCanvasElement;
+    renderer: Renderer;
+    scene: Scene;
+
+    f: number = 0;
+    r: number = 0;
+
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.renderer = new Renderer(canvas);
+        this.scene = new Scene();
+
+        $(document).on("keydown", (event) => {this.handleKeyPressed(event)});
+        $(document).on("keyup", (event) => {this.handleKeyRelease(event)});
+
+        this.canvas.onclick = () => {
+            this.canvas.requestPointerLock();
+        }
+        this.canvas.addEventListener("mousemove", (event) => {this.handleMouseMoved(event)});
+    }
+
+    async initialize() {
+        await this.renderer.Initialize();
+    }
+
+    run = () => {
+        let running = true;
+
+        this.scene.update();
+        this.scene.movePlayer(this.f, this.r);
+        this.renderer.render(this.scene.getPlayer(), this.scene.getTriangles());
+
+        if (running) {
+            requestAnimationFrame(this.run);
+        }
+    }
+
+    handleKeyPressed(event: JQuery.KeyDownEvent) {
+        if (event.code == "KeyW") {
+            this.f = 0.02;
+        }
+        if (event.code == "KeyS") {
+            this.f = -0.02;
+        }
+        if (event.code == "KeyA") {
+            this.r = -0.02;
+        }
+        if (event.code == "KeyD") {
+            this.r = 0.02;
+        }
+    }
+
+    handleKeyRelease(event: JQuery.KeyUpEvent) {
+        if (event.code == "KeyW") {
+            this.f = 0;
+        }
+        if (event.code == "KeyS") {
+            this.f = 0;
+        }
+        if (event.code == "KeyA") {
+            this.r = 0;
+        }
+        if (event.code == "KeyD") {
+            this.r = 0;
+        }
+
+    }
+
+    handleMouseMoved(event: MouseEvent) {
+        this.scene.spinPlayer(event.movementX / 5, event.movementY / -5);
+    }
+}
--- src/models/scene.ts
+++ src/models/scene.ts
@@ -1,5 +1,6 @@
 import { Triangle } from "./triangle";
 import { Camera } from "./camera";
+import { vec3 } from "gl-matrix";
 
 export class Scene {
     triangles: Triangle[];
@@ -18,6 +19,17 @@ export class Scene {
         );
 
         this.player.update();
+    }
+
+    spinPlayer(dx: number, dy: number) {
+        this.player.eulers[2] -= dx;
+        this.player.eulers[2] % 360;
+        this.player.eulers[1] = Math.min(89, Math.max(-89, this.player.eulers[1] + dy));
+    }
+
+    movePlayer(forwards: number, right: number) {
+        vec3.scaleAndAdd(this.player.position, this.player.position, this.player.forwards, forwards);
+        vec3.scaleAndAdd(this.player.position, this.player.position, this.player.right, right);
     }
 
     getPlayer(): Camera {
